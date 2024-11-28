72aae5d7d24ba8a379cbae5ca1a77989bfa44ef9
Mihael <mihaelmiki123@live.com>
2024-11-28 22:12:00 +0100

    .

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,16 +5,16 @@ import {error} from '@sveltejs/kit'
 export async function load() {
 	/** @type {{data: App.Doctor[]}} */
 	// @ts-ignore
-	const { data: doctors } = await db.from('doctors').select('*, institution (*), city (*)').order('rating', {ascending: false});
+	const { data: doctors } = await db.from('doctors').select('*, institution (*), city (*)').order('rating', {ascending: false}).limit(12);
     //if (doctors) {;
 
 		/** @type {{data: App.Institution[]}} */
 		// @ts-ignore
-		const { data: institutions } = await db.from('institutions').select('*, city (*)');
+		const { data: institutions } = await db.from('institutions').select('*, city (*)').order('rating', {ascending: false}).limit(12);
 
 		/** @type {{data: App.City[]}} */
 		// @ts-ignore
-		const { data: cities } = await db.from('cities').select('*');
+		const { data: cities } = await db.from('cities').select('*').limit(12);
 	
 		return {
 			topDoctors: doctors,
