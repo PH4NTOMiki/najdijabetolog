14493b358dc9c2279d79b52a15886a2d26e9ad11
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2024-12-30 00:11:00 +0100

    Update +page.svelte

--- src/routes/doktori/[id]/+page.svelte
+++ src/routes/doktori/[id]/+page.svelte
@@ -190,7 +190,7 @@
             onclick={openModal}
             class="py-2 px-4 bg-blue-600 text-white font-bold rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
-            Dodajte recenziju
+            Dodajte ocjenu i recenziju
         </button>
     </div>
 
