8d11676cac38ed1980397d1136db1c4035f20cca
Mihael <mihaelmiki123@live.com>
2025-01-03 21:23:16 +0100

    .

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,16 +5,16 @@ import {error} from '@sveltejs/kit'
 export async function load() {
 	/** @type {{data: App.Doctor[]}} */
 	// @ts-ignore
-	const { data: doctors } = await db.from('doctors').select('*, institution (*), city (*)').order('rating', {ascending: false}).limit(12);
+	const { data: doctors } = await db.from('doctors').select('*, institution (*), city (*)').order('rating', {ascending: false}).limit(6);
     //if (doctors) {;
 
 		/** @type {{data: App.Institution[]}} */
 		// @ts-ignore
-		const { data: institutions } = await db.from('institutions').select('*, city (*)').order('rating', {ascending: false}).limit(12);
+		const { data: institutions } = await db.from('institutions').select('*, city (*)').order('rating', {ascending: false}).limit(6);
 
 		/** @type {{data: App.City[]}} */
 		// @ts-ignore
-		//const { data: cities } = await db.from('cities').select('*').limit(12);
+		//const { data: cities } = await db.from('cities').select('*').limit(6);
 	
 		return {
 			topDoctors: doctors,
